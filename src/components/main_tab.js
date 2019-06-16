c8914cd18e562bf1b08642497dbd0c6c4b4f7d76
midal2 <midal2@midal2ui-MacBook-Pro.local>
2019-06-16 22:56:32 +0900

    test

--- src/components/main_tab.js
+++ src/components/main_tab.js
@@ -5,6 +5,7 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 import SwipeableViews from 'react-swipeable-views';
 import RecentlyPrograms from './recently_programs';
 import Programs from './programs';
+import Paper from 'material-ui/Paper';
 
 
 const styles = {
@@ -17,6 +18,14 @@ const styles = {
   slide: {
     padding: 10,
   },
+};
+
+const styles2 = {
+  height: 400,
+  width: 600,
+  margin: 20,
+  textAlign: 'center',
+  display: 'inline-block',
 };
 
 export default class MainTab extends Component{
@@ -47,10 +56,12 @@ export default class MainTab extends Component{
           index={this.state.slideIndex}
           onChangeIndex={this.handleChange}
         >
-          <div>
-            <RecentlyPrograms/>
-            <Programs/>
-          </div>
+            <div>
+              <Paper style={styles2} zDepth={1}>
+                <RecentlyPrograms/>
+              </Paper>
+              <Programs/>
+            </div>
           <div style={styles.slide}>
             지금까지 진행한 통계내역
           </div>
